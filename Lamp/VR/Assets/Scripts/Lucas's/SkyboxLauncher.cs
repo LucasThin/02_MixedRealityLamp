a7e08547a6acc769a2b65820e68395ca0ba4ef65
LucasThin <107634089+LucasThin@users.noreply.github.com>
2022-09-27 20:55:49 -0700

    Timer working with debug

---
+++ Lamp/VR/Assets/Scripts/Lucas's/SkyboxLauncher.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject _model;
+
+    [SerializeField] private List<MeshRenderer> _meshRenderers = new List<MeshRenderer>();
+
+    [SerializeField] private float _Time;
+    [SerializeField] private float _duration = 10f;
+    [SerializeField] private bool _modelShowing = false;
+    // Start is called before the first frame update
+
+    private void Start()
+    {
+        _Time = _duration;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        _model.SetActive(true);
+        //Debug.Log(other.gameObject.name);
+
+        for (int i = 0; i < _meshRenderers.Count; i++)
+        {
+            _meshRenderers[i].enabled = false;
+        }
+
+        _modelShowing = true;
+
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        _model.SetActive(false);
+        
+        for (int i = 0; i < _meshRenderers.Count; i++)
+        {
+            _meshRenderers[i].enabled = true;
+        }
+
+        _modelShowing = false;
+    }
+
+    private void Update()
+    {
+        if (_modelShowing)
+        {
+            Debug.Log(_Time);
+            if (_Time > 0.9f)
+            {
+                _Time = _Time - Time.deltaTime;
+            }
+        }
+        else
+        {
+            ResetTimer();
+        }
+    }
+    
+    private void ResetTimer()
+    {
+        _Time = _duration;
+    }
+}

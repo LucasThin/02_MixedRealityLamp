e426c36671f044bc5ba752af07d05a4d44a007ab
Lucaswang8 <hi@madebylucas.co>
2022-09-29 13:33:42 -0700

    added sounds

--- Lamp/VR/Assets/Scripts/Lucas's/Obstacles.cs
+++ Lamp/VR/Assets/Scripts/Lucas's/Obstacles.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
+    public AudioClip audioclip;
+    public AudioSource source;
     // Start is called before the first frame update
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Obstacles"))
         {
+            source = other.gameObject.GetComponent<AudioSource>();
+            audioclip = source.clip;
+            source.PlayOneShot(audioclip);
             Debug.Log("Saber hit");
             Destroy(other.gameObject);
         }
